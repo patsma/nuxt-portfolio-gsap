284b47764d215a5f9d51020dc0b9237774be7332
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-11-17 00:57:54 +0100

    Update app.vue to enable gradient and cursor trail components
    - Uncomment FluidGradient component to restore fluid gradient background
    - Uncomment CursorTrail component to enable smooth cursor trail effect

--- app/app.vue
+++ app/app.vue
@@ -22,12 +22,12 @@ onMounted(() => {
 
 <template>
   <!-- Fluid gradient background (z-0, behind everything) -->
-  <!-- <FluidGradient /> -->
+  <FluidGradient />
 
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 
   <!-- Smooth cursor trail effect (z-50, on top) -->
-  <!-- <CursorTrail /> -->
+  <CursorTrail />
 </template>
